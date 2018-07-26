ee481008440cbdddb3ea59f785f57261ef6caada
wangxirui <wangxirui@shuidihuzhu.com>
2018-07-26 17:12:52 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+import maps from './map'
+
+Vue.use(vueRouter)
+
+export default new vueRouter({
+  routes:maps
+})
---
+++ src/router/map.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    path: '/',
+    component: (resolve)=>{
+      require(['../views'])
+    }
+  }
+]
