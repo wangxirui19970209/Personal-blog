43cf497bff517a050743251d3e22a40622492b56
wangxirui <wangxirui@shuidihuzhu.com>
2018-09-26 14:12:34 +0800

    添加json数据

--- src/components/leftMenu.vue
+++ src/components/leftMenu.vue
@@ -10,23 +10,15 @@
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b">
-          <el-submenu index="1">
+          <el-submenu v-for="(item,index) in menuConfig" :index="item.folderId" :key="index">
             <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>{{item.label}}</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item  v-for="(children,num) in menuConfig[index].children" :index="`${index}-${num}`" :key="num">
+                <a :href="children.url">{{children.label}}</a>
+              </el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -35,15 +27,17 @@
 </template>
 
 <script>
+  import menuConfig from './../config/menuConfig'
   export default {
     name: "leftMenu",
     mounted() {
       this.$api.index.project().then((res)=>{
       })
-// console.log(this.$api.project)
+      console.log(menuConfig.children)
     },
     data() {
       return {
+        menuConfig: menuConfig
       }
     },
     methods: {
