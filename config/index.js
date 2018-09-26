ae4e9356d3c91660e7332d270c0c5c1291164c25
wangxirui <wangxirui@shuidihuzhu.com>
2018-09-26 18:57:32 +0800

    修改路径

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: './Personal-blog/',
     proxyTable: {},
 
     // Various Dev Server settings
